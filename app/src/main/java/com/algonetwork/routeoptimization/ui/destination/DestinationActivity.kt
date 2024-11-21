6b60f8187b2f153d0676dc417e5de455aec3bb41
thoriqmu <tmuchlishin@gmail.com>
2024-11-21 15:59:09 +0700

    Create Destination Layout

---
+++ app/src/main/java/com/algonetwork/routeoptimization/ui/destination/DestinationActivity.kt
@@ -0,0 +1,21 @@
+package com.algonetwork.routeoptimization.ui.destination
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.algonetwork.routeoptimization.R
+
+class DestinationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_destination)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
