c44b603b27e14b16a3dd48ec62e16cf1d0ba2e0d
Naufal Ammar <naufnaufalamr@gmail.com>
2024-11-21 12:45:04 +0700

    Create History Trip Layout

--- app/src/main/java/com/algonetwork/routeoptimization/MainActivity.kt
+++ app/src/main/java/com/algonetwork/routeoptimization/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        supportActionBar?.hide()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
